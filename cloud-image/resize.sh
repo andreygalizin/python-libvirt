f16a5920586f984482aa6c8db73f42ebf2aa64dd
andreygalizin <andrey.galizin@gmail.com>
2023-01-08 00:02:43 -0500

    cloud-image folder added

---
+++ cloud-image/resize.sh
@@ -0,0 +1,5 @@
+## sudo su
+sudo netplan --debug apply
+growpart /dev/vda 1
+resize2fs /dev/vda1
+
---
+++ cloud-image/vm.sh
@@ -0,0 +1,17 @@
+## Source:
+## https://serverascode.com/2018/06/26/using-cloud-images.html
+
+# Download the image
+wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+# Enlarge Virtual Disk
+qemu-img info bionic-server-cloudimg-amd64.img
+qemu-img resize bionic-server-cloudimg-amd64.img 20G
+# Set Root Password
+virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:coolpass
+# Remove Cloud Init
+virt-customize -a bionic-server-cloudimg-amd64.img --uninstall cloud-init
+# Define and Start the VM
+virsh define vm.xml
+virsh start vm_name
+# Access Console -> Handy dandy virsh console 
+virsh console vm_name
