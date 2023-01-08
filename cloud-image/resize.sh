5c991eb28d742b5ec7e897668ff44ae31052ef57
andreygalizin <andrey.galizin@gmail.com>
2023-01-08 17:43:12 -0500

    sample XML files added

--- cloud-image/resize.sh
+++ cloud-image/resize.sh
@@ -1,5 +1,5 @@
 ## sudo su
-sudo netplan --debug apply
+netplan --debug apply
 growpart /dev/vda 1
 resize2fs /dev/vda1
 
--- cloud-image/vm.sh
+++ cloud-image/vm.sh
@@ -6,12 +6,12 @@ wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64
 # Enlarge Virtual Disk
 qemu-img info bionic-server-cloudimg-amd64.img
 qemu-img resize bionic-server-cloudimg-amd64.img 20G
-# Set Root Password
-virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:coolpass
-# Remove Cloud Init
-virt-customize -a bionic-server-cloudimg-amd64.img --uninstall cloud-init
-# Define and Start the VM
+# # Set Root Password
+sudo virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:coolpass
+# # Remove Cloud Init
+sudo virt-customize -a bionic-server-cloudimg-amd64.img --uninstall cloud-init
+# # Define and Start the VM
 virsh define vm.xml
-virsh start vm_name
-# Access Console -> Handy dandy virsh console 
-virsh console vm_name
+# virsh start vm_name
+# # # Access Console -> Handy dandy virsh console 
+# virsh console vm_name
